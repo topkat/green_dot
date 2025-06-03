07afac8d0dfe78a2b504dc7ffa093612fcf12cc6
TOPKAT <webmaster@nicewebagence.com>
2025-06-03 22:20:31 +0200

    refactor: simplify type definitions in index.generated.ts and update package.json dependency
    - Streamlined type definitions in index.generated.ts by removing unnecessary complexity and consolidating types.
    - Updated the green_dot dependency version in package.json from 0.0.41 to 0.0.44 for improved functionality.

--- src/cache/dbs/index.generated.ts
+++ src/cache/dbs/index.generated.ts
@@ -1,42 +1,19 @@
 
-import { MergeMultipleObjects } from 'typescript-generic-types'
-import { UserAdditionalFieldsRead, UserAdditionalFieldsWrite } from '../../security/userAndConnexion/userAdditionalFields'
-import { AllModels as AdminAllModels } from './admin.modelTypes.generated'
-import { AllModels as BangkAllModels } from './bangk.modelTypes.generated'
-import { AllModels as WebsiteAllModels } from './website.modelTypes.generated'
 
+export type ModelsWithDbNamesAndReadWrite = Record<string, any>
 
+export type DbIds = Record<string, string>
 
-export type ModelsWithDbNamesAndReadWrite = {
-    admin: AdminAllModels
-    bangk: { [K in keyof BangkAllModels]: K extends 'user' ? BangkAllModels[K] & { Read: UserPermissionFields & UserAdditionalFieldsRead, Write: Partial<UserPermissionFields & UserAdditionalFieldsWrite> } : BangkAllModels[K] }
-    website: WebsiteAllModels
-}
+export type AllDbIds = string
 
-export type DbIds = {
-    admin: 'admin'
-    bangk: 'bangk'
-    website: 'website'
-}
+export type MainDbName = string
 
-export type AllDbIds = DbIds[keyof DbIds]
+export type ModelsWithReadWrite = Record<string, any>
 
-export type MainDbName = 'bangk'
-
-export type ModelsWithReadWrite = MergeMultipleObjects<ModelsWithDbNamesAndReadWrite>
-
-/** With this getter you can safely use your model types anywhere (even in db folders).
-If you use straight type like ```User```, it may error when you are in a database folder
- * @example ```type User = ModelTypes['user']```
-*/
-export type ModelTypes = {
-    [K in keyof ModelsWithReadWrite]: ModelsWithReadWrite[K]['Read']
-} & {
-    [K in keyof ModelsWithReadWrite as `${K & string}Write`]: ModelsWithReadWrite[K]['Write']
-}
+export type ModelTypes = Record<string, Record<string, any>> // type safety
 
 /** All ModelNames for all DB Names: 'modelName1' | 'modelName2'...  */
-export type ModelNames = keyof ModelsWithReadWrite
+export type ModelNames = string
 
 /** ModelNames for DB { [dbName]: 'modelName1' | 'modelName2'... } */
-export type ModelNamesForDb = { [K in keyof ModelsWithDbNamesAndReadWrite]: keyof ModelsWithDbNamesAndReadWrite[K] }
+export type ModelNamesForDb = Record<string, string>
